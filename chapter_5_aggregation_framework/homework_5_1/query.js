29ddf7d2bae0898c28f2bd7be4f6538892269e12
mkunikowski <michal.kunikowski@gmail.com>
2015-02-14 17:49:00 +0100

    week6

---
+++ chapter_5_aggregation_framework/homework_5_1/query.js
@@ -0,0 +1,1 @@
+db.posts.aggregate([{ $project: {_id: 0, author: '$comments.author'} }, { $unwind: '$author' }, { $group: { _id: '$author', comments: { $sum: 1 } } }, { $sort: { comments: -1 } }, { $limit: 1 }])
